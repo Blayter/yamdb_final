0c0889c0b7e6ba97f15e088324f4c1d8045207ec
Sergey <blayter@yandex.ru>
2022-03-24 14:15:52 +0300

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="SUP3RS3CR3TK3YF0RMYPR0J3CT")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'web', ]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="127.0.0.1").split(' ')
 
 
 INSTALLED_APPS = [
